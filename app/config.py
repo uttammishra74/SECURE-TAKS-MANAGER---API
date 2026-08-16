308fd5a3a361ecfa9b74fdf5494f4d8cd57bf7c9
Uttam Mishra <ayushmishra47893@gmail.com>
2026-08-17 01:42:52 +0530

    refactor : Update config and .env from system.

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-host = os.getenv("DB_HOST")
-user = os.getenv("DB_USER")
-name = os.getenv("DB_NAME")
-password = os.getenv("DB_PASSWORD")
-port = os.getenv("DB_PORT", "3360")
+host = os.getenv("DB_HOST", "host.docker.internal")
+user = os.getenv("DB_USER", "root")
+name = os.getenv("DB_NAME", "secure_task_manager")
+password = os.getenv("DB_PASSWORD", "")
+port = os.getenv("DB_PORT", "3306")
